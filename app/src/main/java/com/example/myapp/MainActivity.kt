3fb803d6a960766ff18005454d54d1a4f1777b58
Pauloohg <188387@upf.br>
2025-04-05 21:07:05 -0300

    adicionar a lógica da MainActivity

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.myapp.screens.MainScreen
-import com.example.myapp.screens.SecondScreen
+import com.example.myapp.ui.screens.ConfirmationScreen
+import com.example.myapp.ui.screens.MainScreen
+import com.example.myapp.ui.screens.SecondScreen
+import com.example.myapp.ui.screens.ThirdScreen
+import com.example.myapp.viewmodel.ProblemaViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +26,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
     val navController = rememberNavController()
+    val viewModel: ProblemaViewModel = viewModel()
+
     NavHost(navController = navController, startDestination = "mainScreen") {
         composable("mainScreen") { MainScreen(navController) }
         composable("secondScreen") { SecondScreen(navController) }
+        composable("mapScreen/{severity}") { backStackEntry ->
+            val severity = backStackEntry.arguments?.getString("severity") ?: ""
+            ThirdScreen(navController, severity, viewModel)
+        }
+        composable("confirmationScreen") {
+            ConfirmationScreen(viewModel)
+        }
     }
 }
